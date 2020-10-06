ec76cdd5e361d1c4c68138a3e194ad0235fdf2cd
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-06 09:53:06 -0400

    Update Main.java
    creates multiplication table

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * prints out a multiplication table for a number
+ * @Chad 
  */
 public class Main {
 
@@ -9,7 +10,16 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates Scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    // asks user for numeber and stores it 
+    System.out.println("Please enter an integer to create a multiplication table for");
+    int number = input.nextInt();
+    //while the count is below 12, it'll print out whatever the inputted number is, multiplied by whatever the count is (1-12)
+    for (int count = 1; count < 12; count++){
+      int product = count * number;
+      System.out.println(count + " x " + number + " = " + product);
+    }
   }
 }
